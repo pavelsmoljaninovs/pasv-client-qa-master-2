9af89a89f5b24462cda30a3cd946b4939ffc481d
pavelsmoljaninovs <pavel.smoljaninov@yahoo.com>
2019-11-04 20:46:51 -0500

    update

---
+++ test/specs/user/_data/data.js
@@ -0,0 +1,23 @@
+const user = {
+  admin: {
+    email: 'admin@test.com',
+    password: '11111',
+    id: '5d687e4983d5d600380e6b51',
+    firstName: 'Ruslan',
+    lastName: 'Admin',
+    phone: '12312312334',
+  },
+  student: {
+    email: 'student_viktor@test.com',
+    password: '11111',
+    // id: '5d687e4983d5d600380e6b51',
+    firstName: 'Test',
+    lastName: 'Student',
+    phone: '12312312334',
+    about: 'About Test Student About Test Student About Test Student About Test Student',
+    goals: 'About Test Student About Test Student About Test Student About Test Student',
+    englishLevel: 'Elementary',
+  },
+};
+
+module.exports = { user };
